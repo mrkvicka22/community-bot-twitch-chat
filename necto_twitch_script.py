c14adcaf053319951e2b0bd700125d80dd9741a8
Matej <matej.novosad.mn@gmail.com>
2022-04-28 18:15:09 +0200

    did a lot of stuff

--- necto_twitch_script.py
+++ necto_twitch_script.py
@@ -1,98 +1,75 @@
 from rlbot.agents.base_script import BaseScript
-import requests
-import json
-import asyncio
+from twitchAPI.pubsub import PubSub
+from twitchAPI.twitch import Twitch, PredictionStatus
+from twitchAPI.types import AuthScope
+from twitchAPI.oauth import UserAuthenticator
+from uuid import UUID
 
 
 class RLBotTwitchScript(BaseScript):
-    def __init__(self, broadcaster_id):
+    def __init__(self):
         super().__init__("RLBotTwitchScript")
-        self.broadcaster_id = broadcaster_id
+
         # keys and secrets and stuff
         with open("client_data.txt", "r") as f:
-            self.client_id, self.client_secret, self.user, self.oauth = [line.strip().split("=")[1] for line in
-                                                                         f.readlines()]
+            self.CLIENT_ID, self.CLIENT_SECRET, self.USER = [line.strip().split("=")[1] for line in
+                                                             f.readlines()]
+        # twitch api innit
+        self.twitch = Twitch(self.CLIENT_ID, self.CLIENT_SECRET)
+        self.target_scope = [AuthScope.CHANNEL_READ_REDEMPTIONS,
+                             AuthScope.CHANNEL_MANAGE_POLLS,
+                             AuthScope.CHANNEL_MANAGE_PREDICTIONS,
+                             AuthScope.CHANNEL_READ_POLLS]
+        self.auth = UserAuthenticator(self.twitch, self.target_scope, force_verify=False)
+        self.token, self.refresh_token = self.auth.authenticate()
+        self.twitch.authenticate_app(self.target_scope)
+        self.twitch.set_user_authentication(self.token, self.target_scope, self.refresh_token)
+        self.user_id = self.twitch.get_users(logins=[self.USER])['data'][0]['id']
+
+        # starting listening to public events
+        self.pubsub = PubSub(self.twitch)
+
         # rlbot innit
         self.packet = self.wait_game_tick_packet()
         self.f_packet = self.get_field_info()
 
-    def _start_prediction(self, channel_name: str, prediction_title: str, prediction_outcomes,
-                          prediction_window: int):
-        # create a bet (twitch api)
-        headers = {
-            "Authorization": self.user + " " + self.oauth,
-            "Client-ID": self.client_id,
-            'Content-Type': 'application/json',
-        }
-        data = {"broadcaster_id": self.broadcaster_id,
-                "title": prediction_title,
-                "outcomes": prediction_outcomes,
-                "prediction_window": prediction_window
-                }
-        response = requests.post(f"https://api.twitch.tv/{channel_name}/predictions", data=data, headers=headers)
-        if response.status_code == 200:
-            print("Created prediction successfully")
+        # betting
+        self.n_goals = 50
+
+    def goals_check_condition(self, starting_score):
+        current_scores = self._get_scores()
+        if starting_score[0] + 5 <= current_scores[0]:  # might break cos pizza
+            # blue wins the prediction
+            return 0
+        elif starting_score[1] + 5 <= current_scores[1]:
+            # orange wins the prediction
+            return 1
         else:
-            print("Failed to start prediction!")
-        response_json = json.load(response.json())
-        prediction_id = response_json["data"]["id"]
-        blue_outcome_id, pink_outcome_id = [outcome["id"] for outcome in response_json["data"]["outcomes"]]
-        return prediction_id, blue_outcome_id, pink_outcome_id
-
-    def _end_prediction(self, prediction_id, result_id):
-        # end bet (twitch api)
-        headers = {
-            'Authorization': 'Bearer cfabdegwdoklmawdzdo98xt2fo512y',
-            'Client-Id': self.client_id,
-        }
-
-        json_data = {
-            'broadcaster_id': self.broadcaster_id,
-            'id': prediction_id,
-            'status': 'RESOLVED',
-            'winning_outcome_id': result_id,
-        }
-
-        response = requests.patch('https://api.twitch.tv/helix/predictions', headers=headers, json=json_data)
-        if response.status_code == 200:
-            print("Ended prediction successfully")
-        else:
-            print("Failed to end prediction!")
-
-    async def goals_check_condition(self, starting_score):
-        # wait until condition(Blue was first to score 5 goals) is met (rlbot)
-        while True:
-            current_scores = self._get_scores()
-            if starting_score[0] + 5 <= current_scores[0]:  # might break cos pizza
-                # blue wins the prediction
-                return 0
-            elif starting_score[1] + 5 <= current_scores[1]:
-                # orange wins the prediction
-                return 1
-            else:
-                print("No-one won yet")
-            await asyncio.sleep(1)
-
-    async def betting(self, channel_name, prediction_title, prediction_outcomes, prediction_window: int = 120):
-        starting_scores = self._get_scores()
-
-        t = self._start_prediction(channel_name, prediction_title, prediction_outcomes, prediction_window)
-        prediction_id, blue_outcome_id, pink_outcome_id = t
-        result = await self.goals_check_condition(starting_scores)
-
-        if result:
-            result_id = pink_outcome_id
-        else:
-            result_id = blue_outcome_id
-
-        self._end_prediction(prediction_id, result_id)
-        print("it worked?")
+            return None
 
     def _get_scores(self):
         scores = [team.score for team in self.packet.teams]
         return scores
 
+    def _handle_channel_points(self, uuid: UUID, data: dict):
+        raise NotImplementedError
+
+    def _handle_end_prediction(self, prediction_id, outcomes, result):
+        winning_outcome_id = outcomes[result]["id"]
+        self.twitch.end_prediction(self.user_id, prediction_id, PredictionStatus.RESOLVED, winning_outcome_id)
+
+    def _handle_create_prediction(self):
+        response = self.twitch.create_prediction(self.user_id, f"Who is going to score {self.n_goals} goals first",
+                                                 ["Blue", "Orange"], 120)
+        return response["data"]["outcomes"]
+
     def run(self):
+        prediction_active = False
+        prediction_id = None
+        outcomes = None
+        self.pubsub.start()
+        self.pubsub.listen_channel_points(self.user_id, callback_func=self._handle_channel_points)
+        starting_score = self._get_scores()
         while True:
             self.packet = self.wait_game_tick_packet()
             if not self.f_packet:
@@ -100,8 +77,16 @@ class RLBotTwitchScript(BaseScript):
             if self.packet.game_info.is_match_ended:
                 break
 
+            # handle predictions
+            if prediction_active:
+                result = self.goals_check_condition(starting_score)
+                if result is not None:
+                    self._handle_end_prediction(prediction_id, outcomes, result)
+            else:
+                starting_score = self._get_scores()
+                outcomes = self._handle_create_prediction()
+
 
 if __name__ == "__main__":
-    BROADCASTER_ID = "141981764"
-    script = RLBotTwitchScript(BROADCASTER_ID)
+    script = RLBotTwitchScript()
     script.run()
